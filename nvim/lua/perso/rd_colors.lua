59162f93ea02b9b0fb9a0f85d7a86e311955d702
196884 <196884@users.noreply.github.com>
2025-01-30 22:24:41 -0500

    Update rd_colors.lua

--- nvim/lua/perso/rd_colors.lua
+++ nvim/lua/perso/rd_colors.lua
@@ -1,3 +1,6 @@
-vim.api.nvim_set_hl(0, "VertSplit",         {fg="black", bg="red"})
-vim.api.nvim_set_hl(0, "WinSeparator",      {fg="black", bg="#005f87"})
-
+vim.api.nvim_set_hl(0, "VertSplit",                    {fg="black", bg="red"})
+vim.api.nvim_set_hl(0, "WinSeparator",                 {fg="black", bg="#005f87"})
+vim.api.nvim_set_hl(0, "RdStatusLineModeAccent",       {fg="black", bg="#00acc1"})
+vim.api.nvim_set_hl(0, "RdStatusLineInsertModeAccent", {fg="black", bg="#ff6e40"})
+vim.api.nvim_set_hl(0, "RdStatusLineAccent",           {fg="black", bg="white"})
+vim.api.nvim_set_hl(0, "RdStatusLineNCAccent",         {fg="black", bg="darkgrey"})
