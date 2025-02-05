373906e4ef3c54a51dd21c831ecf0ba05f69f420
196884 <196884@users.noreply.github.com>
2025-02-05 14:47:00 +0100

    Update rd_fzf-lua.lua
    grep_(cword/cWORD) shortcuts

--- nvim/lua/perso/rd_fzf-lua.lua
+++ nvim/lua/perso/rd_fzf-lua.lua
@@ -1,4 +1,6 @@
 vim.keymap.set('n', '<leader>f/', rd_root_cmd_wrapper(':FzfLua grep cwd={RD_ROOT}<CR>'), { desc = 'FzfLua grep' })
 vim.keymap.set('n', '<leader>ff', rd_root_cmd_wrapper(':FzfLua files cwd={RD_ROOT}<CR>'), { desc = 'FzfLua files' })
+vim.keymap.set('n', '<leader>fcw', rd_root_cmd_wrapper(':FzfLua grep_cword cwd={RD_ROOT}<CR>'), { desc = 'FzfLua grep_cword' })
+vim.keymap.set('n', '<leader>fcW', rd_root_cmd_wrapper(':FzfLua grep_cWORD cwd={RD_ROOT}<CR>'), { desc = 'FzfLua grep_cWORD' })
 vim.keymap.set('n', '<leader>fr', ':FzfLua resume<CR>', { desc = 'FzfLua resume' })
 
